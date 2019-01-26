16f215743bcf7ad67edab5812f9614695638e108
lsh-hhh <lshandhsl@outlook.com>
2019-01-26 13:23:13 +0800

    Initial commit

---
+++ src/my/lsh/first/HelloWorld.java
@@ -0,0 +1,20 @@
+package my.lsh.first;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //hfgkhjfgjhg
+        System.out.println("Hello World");
+        test();
+    }
+
+    public static void test(){
+        System.out.println();
+        System.out.println("true = " + true);
+        System.out.println("HelloWorld.test");
+        System.out.println("");
+
+        int x = 10;
+        System.out.println(x);
+
+    }
+}
---
+++ webApplication/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: LSH
+  Date: 2019/1/26
+  Time: 10:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>LSH测试用</title>
+  </head>
+  <body>
+    <h1 style="color: red">Hello IDEA</h1>
+  </body>
+</html>
